97ae6a66ea73a3c31fdc2245150557f85509531a
Salvatore <workstudioms@gmail.com>
2025-06-22 16:09:42 +0200

    feat: Milestone 1 complete. todo: form validation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,165 @@
 import { useState } from 'react'
 
-// import './App.css'
+import './App.css'
+
 
 function App() {
 
+  const [form, setForm] = useState({
+    name: "",
+    username: "",
+    password: "",
+    specialization: "",
+    experience: "",
+    description: ""
+  });
+
+
+  const [errors, setErrors] = useState({});
+
+
+  // Validazione errori
+  const validateValues = (values) => {
+    const errors = {};
+
+    if (!values.name.trim()) {
+      errors.name = "Inserisci il tuo nome";
+    }
+
+    if (!values.username.trim()) {
+      errors.username = "Lo username deve contene almeno 6 caratteri. Spazi e simboli non sono permessi";
+    }
+
+    if (values.password.length < 6) {
+      errors.password = "La password deve contenere almeno 8 caratteri, 1 lettera, 1 numero e 1 simbolo";
+    }
+
+    if (!values.specialization) {
+      errors.specialization = "Seleziona una specializzazione";
+    }
+
+    if (Number(values.experience) % 2 !== 0) {
+      errors.experience = "Gli anni di esperienza devono essere pari";
+    }
+
+    if (!values.description.trim()) {
+      errors.description = "La descrizione deve contenere tra 100 e 1000 caratteri. Spazi iniziali e finali non sono permessi";
+    }
+
+    console.log(errors);
+
+    return errors;
+  };
+
+
+  const handleChange = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+
+    event.preventDefault();
+
+    const validationErrors = validateValues(form);
+
+    setErrors(validationErrors);
+
+    if (Object.values(validationErrors).length > 0) {
+      console.log("Errore, verifica che tutti i campi siano corretti")
+      return;
+    }
+
+    console.log(form);
+
+  };
+
+
   return (
+
     <>
+
+      <form className='custom-form' onSubmit={handleSubmit}>
+
+        <label>Nome e Cognome:
+          <input type="text"
+            value={form.name}
+            name="name"
+            onChange={handleChange}
+          />{errors.name && <span className="error">{errors.name}</span>}
+        </label>
+
+        <label>Username:
+          <input type="text"
+            value={form.username}
+            name="username"
+            onChange={handleChange}
+          />{errors.username && <span className="error">{errors.username}</span>}
+        </label>
+
+        <label>Password:
+          <input type="text"
+            value={form.password}
+            name="password"
+            onChange={handleChange}
+          />{errors.password && <span className="error">{errors.password}</span>}
+        </label>
+
+        <label>Specializzazione:
+          <select
+            value={form.specialization}
+            name="specialization"
+            onChange={handleChange}
+          >
+            <option value="">Seleziona una specializzazione</option>
+            <option value="Full Stack">Full stack</option>
+            <option value="Frontend">Frontend</option>
+            <option value="Backend">Backend</option>
+          </select>{errors.specialization && <span className="error">{errors.specialization}</span>}
+        </label>
+
+        <label>Anni di esperienza:
+          <input type="number"
+            value={form.experience}
+            name="experience"
+            onChange={handleChange}
+            min="0" max="100"
+          />{errors.experience && <span className="error">{errors.experience}</span>}
+        </label>
+
+        <label>Breve descrizione sullo sviluppatore:
+          <textarea
+            value={form.description}
+            name="description"
+            onChange={handleChange}
+          />{errors.description && <span className="error">{errors.description}</span>}
+        </label>
+
+        <button type="submit">Submit</button>
+
+      </form>
+
     </>
+
   )
+
 }
 
 export default App
+
+
+
+// Milestone 1: Creare un Form con Campi Controllati
+
+// Crea un form di registrazione con i seguenti campi controllati (gestiti con useState):
+
+// ✅ Nome completo (input di testo)
+
+// ✅ Username (input di testo)
+
+// ✅ Password (input di tipo password)
+
+// ✅ Specializzazione (select con opzioni: "Full Stack", "Frontend", "Backend")
+
+// ✅ Anni di esperienza (input di tipo number)
+
+// ✅ Breve descrizione sullo sviluppatore (textarea)
